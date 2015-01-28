43733d7388adb77898ef3f8026d6245dbec5ca3b
yuxiang-zhou <yz4009@antony.doc.ic.ac.uk>
2015-01-28 14:56:22 +0000

    Tiny Bridge to Matlab

---
+++ menpofit/deformationfield/MatlabExecuter.py
@@ -0,0 +1,21 @@
+import subprocess, os
+
+
+class MatlabExecuter(object):
+    def __init__(self):
+        self.exe_str = '-nodisplay -nosplash -nodesktop -r "{};exit;"'
+        self._dir = '~'
+
+    def _run(self, command):
+        os.chdir(self._dir)
+        p = subprocess.Popen(['matlab', self.exe_str.format(command)])
+        p.wait()
+
+    def run_script(self, script):
+        self._run('run({})'.format(script))
+
+    def run_function(self, function):
+        self._run(function)
+
+    def cd(self, path):
+        self._dir = path if path[0] == '/' else self._dir + path
